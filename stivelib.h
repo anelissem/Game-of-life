4a609453a22c6098790cec2d13764fcb78265fe9
anelissem <anelissemaria10@yahoo.com>
2025-04-02 21:31:05 +0300

    change the original library used:added the functions used for the lists that save the position of the cells that modify their state

--- stivelib.h
+++ stivelib.h
@@ -3,9 +3,9 @@
 
 struct list{
     int l,c;
-    struct lista *next;
+    struct list *next;
 };
-typedef struct lista LIST;
+typedef struct list LIST;
 struct stack{
     LIST *generation;
     struct stack *next;
@@ -16,5 +16,9 @@ void push(GEN **stacktop,LIST *generatie);
 int isEmpty(GEN *stacktop);
 LIST* pop(GEN **stacktop);
 void deleteStack(GEN **stacktop);
+void AddAtBeginning(LIST **head,int l,int c);
+void AddAtEnd(LIST **head,int l,int c);
+void DeleteList(LIST **head);
+void print_list(LIST *head,const char* argv[],int K);
 
 #endif
