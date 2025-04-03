6addb93e9244eaa6feef53fb430e34f74cebe4c3
anelissem <anelissemaria10@yahoo.com>
2025-04-03 12:40:34 +0300

    creating the second task main program: substracting from the main file the functions specially created for the second task and making a new C file for a more clear view of the code

---
+++ task2.c
@@ -0,0 +1,158 @@
+#include "stivelib.h"
+#include <stdio.h>
+#include <stdlib.h>
+struct celula
+{
+  char state;
+  int neighbors;
+};
+typedef struct celula CEL;
+void rules_stack(CEL **matrix, int N, int M, int K,GEN **stacktop, const char *argv[])
+{
+  int change_whole = 0;
+  while (change_whole < K)
+  {
+    /// Reset neighbors count
+    for (int i = 0; i < N; i++)
+    {
+      for (int j = 0; j < M; j++)
+      {
+        matrix[i][j].neighbors = 0;
+      }
+    }
+    /// Count neighbors
+    for (int i = 0; i < N; i++)
+    {
+      for (int j = 0; j < M; j++)
+      {
+        if (matrix[i][j].state == 'X')
+        {
+          if (j - 1 >= 0)
+            matrix[i][j - 1].neighbors++;
+          if (j + 1 < M)
+            matrix[i][j + 1].neighbors++;
+          if (i - 1 >= 0 && j - 1 >= 0)
+            matrix[i - 1][j - 1].neighbors++;
+          if (i - 1 >= 0 && j + 1 < M)
+            matrix[i - 1][j + 1].neighbors++;
+          if (i - 1 >= 0)
+            matrix[i - 1][j].neighbors++;
+          if (i + 1 < N && j - 1 >= 0)
+            matrix[i + 1][j - 1].neighbors++;
+          if (i + 1 < N && j + 1 < M)
+            matrix[i + 1][j + 1].neighbors++;
+          if (i + 1 < N)
+            matrix[i + 1][j].neighbors++;
+        }
+      }
+    }
+    int changes = 0;
+    ///creating the list representing the new positions of the cels that change
+    LIST *head=NULL;
+    int ok=0;///pt a verifica daca am bagat sau nu primul element in lista
+    for (int i = 0; i < N; i++)
+    {
+      for (int j = 0; j < M; j++)
+      {
+        if (matrix[i][j].state == 'X')
+        {
+          if (matrix[i][j].neighbors < 2)
+          { /// subpopulare
+            if(ok==0){
+              AddAtBeginning(&head,i,j);
+              ok=1;
+            }
+            else
+              AddAtEnd(&head,i,j);
+            matrix[i][j].state = '+';
+            changes++;
+          }
+          else if (matrix[i][j].neighbors > 3)
+          { /// suprapopulare
+            if(ok==0){
+              AddAtBeginning(&head,i,j);
+              ok=1;
+            }
+            else
+              AddAtEnd(&head,i,j);
+            matrix[i][j].state = '+';
+            changes++;
+          }
+        }
+        else if (matrix[i][j].state == '+')
+        {
+          if (matrix[i][j].neighbors == 3)
+          { /// reproducere
+            if(ok==0){
+              AddAtBeginning(&head,i,j);
+              ok=1;
+            }
+            else
+              AddAtEnd(&head,i,j);
+            matrix[i][j].state = 'X';
+            changes++;
+          }
+        }
+      }
+    }
+    if (changes != 0)
+    {
+      change_whole++;
+      push(stacktop,head);
+      print_list(head,argv,change_whole);
+    }
+    else
+    {
+      break;
+    }
+    DeleteList(&head);
+  }
+}
+int main(int argc, const char *argv[])
+{
+  FILE *f;
+  if ((f = fopen(argv[1], "r")) == NULL)
+  {
+    printf("Error at the opening of the input file");
+    return 1;
+  }
+  int T, N, M, K;
+  fscanf(f, "%d\n", &T);
+  fscanf(f, "%d %d\n", &N, &M);
+  fscanf(f, "%d\n", &K);
+  /// allocating memory for the matrix that represents the game table
+  CEL **matrix;
+  matrix = (CEL **)malloc(N * sizeof(CEL *));
+  if (matrix == NULL)
+  {
+    printf("Error at the momory allocation for the entire matrix\n");
+  }
+  for (int j = 0; j < N; j++)
+  {
+    matrix[j] = (CEL *)malloc(M * sizeof(CEL));
+    if (matrix[j] == NULL)
+    {
+      printf("Error at the momory allocation for the rows of the matrix\n");
+      return 1;
+    }
+  }
+  for (int j = 0; j < N; j++)
+    for (int q = 0; q < M; q++)
+    {
+      fscanf(f, " %c", &matrix[j][q].state);
+      matrix[j][q].neighbors = 0;
+    }
+    if (T == 2)
+  {
+    GEN *stacktop = NULL;
+    rules_stack(matrix,N,M,K,&stacktop,argv);
+    deleteStack(&stacktop);
+  }
+  for (int j = 0; j < N; j++)
+  {
+    free(matrix[j]);
+  }
+  free(matrix);
+  fclose(f);
+  return 0;
+}
