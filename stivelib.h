056b1669fc06308ebc23c70572acb9abde2d298e
anelissem <anelissemaria10@yahoo.com>
2025-04-14 17:52:04 +0300

    updated the library for the program: adding the functions for resolving the bonus task

--- stivelib.h
+++ stivelib.h
@@ -11,14 +11,23 @@ struct stack{
     struct stack *next;
 };
 typedef struct stack GEN;
-
-void push(GEN **stacktop,LIST *generatie);
+struct celula
+{
+  char state;
+  int neighbors;
+};
+typedef struct celula CEL;
+void print(CEL **matrix, int N, int M, const char *argv[]);
+void rules_stack(CEL **matrix, int N, int M, int K,GEN **stacktop, const char *argv[],int T);
+void push(GEN **stacktop,LIST *head);
+LIST *pop(GEN **stacktop);
 int isEmpty(GEN *stacktop);
-LIST* pop(GEN **stacktop);
+void printStack(GEN *stacktop);
 void deleteStack(GEN **stacktop);
 void AddAtBeginning(LIST **head,int l,int c);
 void AddAtEnd(LIST **head,int l,int c);
 void DeleteList(LIST **head);
 void print_list(LIST *head,const char* argv[],int K);
-
+GEN *create_stack_matrix(const char *argv[],CEL **matrix,int N,int M,int K,int T);
+void make_first_generation(const char *argv[],GEN **stacktop,CEL **matrix,int N,int M,int K,int T);
 #endif
