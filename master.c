d9e40a1a3ce0d2b700559281c36c1c07132b31ac
anelissem <anelissemaria10@yahoo.com>
2025-04-14 17:49:19 +0300

    making the bonus task in functions: created functions for the code sequence for the BONUS task

--- master.c
+++ master.c
@@ -26,13 +26,7 @@ int main(int argc, const char *argv[])
             M = strlen(linie) - 1;
     }
     fclose(f);
-    if ((f = fopen(argv[1], "r")) == NULL)
-    {
-        printf("Error at the opening of the input file");
-        return 1;
-    }
-    fscanf(f, "%d\n", &T);
-    fscanf(f, "%d\n", &K);
+    ///alocarea dinamica pentru matrice
     CEL **matrix;
     matrix = (CEL **)malloc(N * sizeof(CEL *));
     if (matrix == NULL)
@@ -50,16 +44,10 @@ int main(int argc, const char *argv[])
                 free(matrix[k]);
             }
             free(matrix);
-            fclose(f);
             return 1;
         }
     }
-    for (int j = 0; j < N; j++)
-        for (int q = 0; q < M; q++)
-        {
-            fscanf(f, " %c", &matrix[j][q].state);
-            matrix[j][q].neighbors = 0;
-        }
+    ///deschid fisierul de output in modul write pentru a sterge testele anterioare
     FILE *out;
     if ((out = fopen(argv[2], "w")) == NULL)
     {
@@ -67,55 +55,9 @@ int main(int argc, const char *argv[])
         return 1;
     }
     fclose(out);
-    GEN *stacktop = NULL;
-    fgets(linie, sizeof(linie), f);
-    fgets(linie, sizeof(linie), f);
-    while (fgets(linie, sizeof(linie), f))
-    {
-        LIST *head = NULL;
-        int lenght = strlen(linie);
-        int pair = 1; // pentru verificarea daca am gasit si linia si coloana pentru element
-        int i = 0;
-        int l, c;
-        for (i = 0; i < lenght && linie[i] != '\n';) {
-            if (linie[i] != ' ') { 
-                int number = 0;
-                int digits = 0;
-                while (i < lenght && linie[i] != ' ' && linie[i] != '\n' && digits < 3) {
-                    number = number * 10 + (linie[i] - '0'); // Construiește numărul
-                    i++;
-                    digits++;
-                }
-                if (pair == 1) {
-                    l = number; 
-                    pair++;
-                } else {
-                    c = number; 
-                    if (head == NULL)
-                        AddAtBeginning(&head, l, c);
-                    else
-                        AddAtEnd(&head, l, c);
-                    pair = 1;
-                }
-            } else {
-                i++; 
-            }
-        }
-        push(&stacktop, head);
-    }
-    while(stacktop!=NULL){
-        LIST *head=pop(&stacktop);
-        while(head != NULL){
-            int l=head->l;
-            int c=head->c;
-            if(matrix[l][c].state=='+')
-                matrix[l][c].state='X';
-            else
-                matrix[l][c].state='+';
-            head=head->next;
-        }
-        DeleteList(&head);
-    }
+    ///creez stiva citita din fisier
+    GEN *stacktop = create_stack_matrix(argv,matrix,N,M,K,T);
+    make_first_generation(argv,&stacktop,matrix,N,M,K,T);
     print(matrix,N,M,argv);
     deleteStack(&stacktop);
     for (int j = 0; j < N; j++)
