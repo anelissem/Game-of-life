7c5e880be1ea8d6a7b927320d96f1819ae55cbc2
anelissem <anelissemaria10@yahoo.com>
2025-05-18 02:39:42 +0300

    fixed dtyle issues

--- stivelib.h
+++ stivelib.h
@@ -1,33 +1,52 @@
 #ifndef STIVELIB
 #define STIVELIB
 
-struct list{
-    int l,c;
+struct list {
+    int l, c;
     struct list *next;
 };
 typedef struct list LIST;
-struct stack{
+
+struct stack {
     LIST *generation;
     struct stack *next;
 };
 typedef struct stack GEN;
-struct celula
-{
-  char state;
-  int neighbors;
+
+struct celula {
+    char state;
+    int neighbors;
 };
 typedef struct celula CEL;
+
+struct arbore {
+    LIST *cells;
+    struct arbore *left, *right;
+};
+typedef struct arbore TREE;
+
+// Function declarations
 void print(CEL **matrix, int N, int M, const char *argv[]);
-void rules_stack(CEL **matrix, int N, int M, int K,GEN **stacktop, const char *argv[],int T);
-void push(GEN **stacktop,LIST *head);
+void rules_stack(CEL **matrix, int N, int M, int K, GEN **stacktop, const char *argv[], int T);
+void push(GEN **stacktop, LIST *head);
 LIST *pop(GEN **stacktop);
 int isEmpty(GEN *stacktop);
 void printStack(GEN *stacktop);
 void deleteStack(GEN **stacktop);
-void AddAtBeginning(LIST **head,int l,int c);
-void AddAtEnd(LIST **head,int l,int c);
+void AddAtBeginning(LIST **head, int l, int c);
+void AddAtEnd(LIST **head, int l, int c);
 void DeleteList(LIST **head);
-void print_list(LIST *head,const char* argv[],int K);
-GEN *create_stack_matrix(const char *argv[],CEL **matrix,int N,int M,int K,int T);
-void make_first_generation(const char *argv[],GEN **stacktop,CEL **matrix,int N,int M,int K,int T);
+void print_list(LIST *head, const char *argv[], int K);
+GEN *create_stack_matrix(const char *argv[], CEL **matrix, int N, int M, int K, int T);
+void make_first_generation(const char *argv[], GEN **stacktop, CEL **matrix, int N, int M, int K, int T);
+TREE *createNode(LIST *head);
+void change_rules(TREE **root, CEL **matrix,const char *argv[], int N, int M, int K, int T);
+void preorder(TREE *root, CEL **matrix, int N, int M, const char *argv[]);
+void delete_tree(TREE **root);
+CEL **make_copy(CEL **matrix, int N, int M);
+void create_list(CEL **matrix, LIST **head, int N, int M);
+void rule_B(CEL **matrix, int N, int M);
+void rule_A(CEL **matrix, int N, int M);
+TREE *create_initial_tree(CEL **matrix, LIST *gen0, int N, int M);
+void count_neighbors(CEL **matrix, int N, int M);
 #endif
